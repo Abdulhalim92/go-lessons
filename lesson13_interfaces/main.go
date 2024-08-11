d27a466a1e6e871653026c05495db67857c3556b
Эргашев Халимджон <abdulhalim1992@gmail.com>
2024-08-11 18:35:17 +0500

    add lesson14

---
+++ lesson13_interfaces/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	introduce()
+	embedding()
+	duck()
+	interfaceValue()
+	nilAssigment()
+	nilChecking()
+	notInitialized()
+	emptyInterface()
+	polymorphism()
+	typeAssertion()
+	typeSwitches()
+}

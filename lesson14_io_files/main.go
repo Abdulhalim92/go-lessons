9db8c094b8fce493961557e94500687425b445b2
Эргашев Халимджон <abdulhalim1992@gmail.com>
2024-08-16 19:38:36 +0500

    add some examples with JSON

--- lesson14_io_files/main.go
+++ lesson14_io_files/main.go
@@ -17,60 +17,80 @@ func main() {
 	//
 	//fmt.Fprintln(out, num)
 
-	// Открываем файл для чтения и записи
-	file, err := os.OpenFile("example.json", os.O_CREATE|os.O_RDWR, 0777)
+	//var address Address = Address{
+	//	Street: "Rudaki",
+	//	City:   "Dushanbe",
+	//}
+	//
+	//var p Person2 = Person2{
+	//	Name:    "Sobirjon",
+	//	Age:     21,
+	//	Email:   "sobirjon@go.dev",
+	//	Address: address,
+	//}
+	//
+	//data, err := json.MarshalIndent(p, "", "\t")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(string(data))
+	//
+	file, err := os.OpenFile("example.json", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	//
+	//n, err := file.Write(data)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Printf("Wrote %d bytes to file.\n", n)
 
-	var p2 []Person
+	jsonData := `{
+	"name": "Sobirjon",
+	"age": 21,
+	"email": "sobirjon@go.dev",
+	"address": {
+		"street": "Rudaki",
+		"city": "Dushanbe"
+		}
+	}`
 
-	// Читаем данные из файла
+	byteData := []byte(jsonData)
+
+	var person Person2
+	err = json.Unmarshal(byteData, &person)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", person)
+
 	data, err := io.ReadAll(file)
 	if err != nil {
 		panic(err)
 	}
 
-	// Если файл не пустой, десериализуем данные
-	if len(data) > 0 {
-		err = json.Unmarshal(data, &p2)
-		if err != nil {
-			panic(err)
-		}
-	}
+	var p2 Person2
 
-	// Добавляем нового человека в массив
-	p := Person{
-		Name: "Said",
-		Age:  12,
-	}
-
-	p2 = append(p2, p)
-
-	// Возвращаем указатель на начало файла
-	_, err = file.Seek(0, 0)
+	err = json.Unmarshal(data, &p2)
 	if err != nil {
 		panic(err)
 	}
 
-	// Сериализуем массив обратно в JSON
-	data, err = json.MarshalIndent(p2, "", "    ")
-	if err != nil {
-		panic(err)
-	}
+	fmt.Printf("%+v\n", p2)
+}
 
-	// Очищаем файл перед записью новых данных
-	err = file.Truncate(0)
-	if err != nil {
-		panic(err)
-	}
+type Person2 struct {
+	Name    string  `json:"name,omitempty"`
+	Age     int     `json:"age,omitempty"`
+	Email   string  `json:"email,omitempty"`
+	Address Address `json:"address,omitempty"`
+}
 
-	// Записываем данные обратно в файл
-	_, err = file.Write(data)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Success")
+type Address struct {
+	Street string `json:"street,omitempty"`
+	City   string `json:"city,omitempty"`
 }

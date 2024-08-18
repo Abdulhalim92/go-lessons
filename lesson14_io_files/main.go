4e716a6e7be58cffac94d79aabe30cd353aedb92
Эргашев Халимджон <abdulhalim1992@gmail.com>
2024-08-18 20:50:36 +0500

    add lesson15

--- lesson14_io_files/main.go
+++ lesson14_io_files/main.go
@@ -81,6 +81,7 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", p2)
+
 }
 
 type Person2 struct {
@@ -93,4 +94,64 @@ type Person2 struct {
 type Address struct {
 	Street string `json:"street,omitempty"`
 	City   string `json:"city,omitempty"`
+}
+
+func rwToFile() {
+	// Открываем файл для чтения и записи
+	file, err := os.OpenFile("example.json", os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var p2 []Person
+
+	// Читаем данные из файла
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// Если файл не пустой, десериализуем данные
+	if len(data) > 0 {
+		err = json.Unmarshal(data, &p2)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Добавляем нового человека в массив
+	p := Person{
+		Name: "Said",
+		Age:  12,
+	}
+
+	p2 = append(p2, p)
+
+	// Возвращаем указатель на начало файла
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// Сериализуем массив обратно в JSON
+	data, err = json.MarshalIndent(p2, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	// Очищаем файл перед записью новых данных
+	err = file.Truncate(0)
+	if err != nil {
+		panic(err)
+	}
+
+	// Записываем данные обратно в файл
+	_, err = file.Write(data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success")
+
 }
